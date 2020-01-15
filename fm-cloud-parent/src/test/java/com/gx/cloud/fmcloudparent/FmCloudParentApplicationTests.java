87f47165d5131a32d86259b4b6c1a3f42ddd2ece
guoxun <2594226416@qq.com>
2020-01-15 10:56:52 +0800

    创建文件管理项目

---
+++ fm-cloud-parent/src/test/java/com/gx/cloud/fmcloudparent/FmCloudParentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gx.cloud.fmcloudparent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FmCloudParentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
