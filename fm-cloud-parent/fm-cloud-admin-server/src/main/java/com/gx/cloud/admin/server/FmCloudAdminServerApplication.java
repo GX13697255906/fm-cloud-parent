2a4ff823b2dd9fcedc98785209e808c01a8c1815
guoxun <2594226416@qq.com>
2020-01-20 17:18:57 +0800

    gateway配置fastjson

--- fm-cloud-parent/fm-cloud-admin-server/src/main/java/com/gx/cloud/admin/server/FmCloudAdminServerApplication.java
+++ fm-cloud-parent/fm-cloud-admin-server/src/main/java/com/gx/cloud/admin/server/FmCloudAdminServerApplication.java
@@ -1,5 +1,6 @@
 package com.gx.cloud.admin.server;
 
+import lombok.extern.slf4j.Slf4j;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
+@Slf4j
 @SpringBootApplication
 @MapperScan(basePackages = {"com.gx.cloud.admin.server.mapper"})
 @EnableFeignClients
@@ -15,6 +17,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class FmCloudAdminServerApplication {
 
     public static void main(String[] args) {
+        log.info("开始加载文件管理模块");
         SpringApplication.run(FmCloudAdminServerApplication.class, args);
     }
 
